f632e29a81a1a5ae9872fd85462493678e74fc88
frydj <fry.derek@icloud.com>
2019-10-15 12:14:11 -0400

    testing commit to my own branch
    testing commit to derek

--- client-react/src/components/Header.js
+++ client-react/src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from '../img/logo.png';
 import '../App.css';
 
+// TESTING COMMIT TO DEREK BRANCH //
+
 function Header() {
   return (
     <div className="header">
